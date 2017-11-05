cb6624cd117ea9932b609fba116e417eb324e226
luobosi <luobosi@2dfire.com>
2017-11-05 18:42:16 +0800

    添加模板文件

--- springboottemplate/CreateDomain.go
+++ springboottemplate/CreateDomain.go
@@ -20,7 +20,8 @@ func CreateDO(tableName string, projectPackage string, tableStructures []allstru
 	templateStr += projectPackage + "\r\n"
 	templateStr += "\r\n"
 	// 这里添加要导入的包文件
-	templateStr += ""
+	templateStr += projectPackage + ".domain.base.Base;"
+	templateStr += "lombok.Data;"
 	templateStr += "\r\n"
 	templateStr += "/**\r\n"
 	// 这里就是用来替换类名
@@ -31,7 +32,7 @@ func CreateDO(tableName string, projectPackage string, tableStructures []allstru
 	// 这里加上 @Data 注解，就可以少写一些 Getter 和 Setter 方法
 	templateStr += "@Data\r\n"
 	// 这里是类名,也需要替换
-	templateStr += "public class " + myutil.ToUpperUnderlinedWordInitialsForClassName(tableName) + "DO {\r\n"
+	templateStr += "public class " + myutil.ToUpperUnderlinedWordInitialsForClassName(tableName) + "DO extends Base{\r\n"
 	templateStr += "\r\n"
 	//////////////////////////////////////////////////////////////////// 这里是需要根据数据库里面的结果生成字段
 
