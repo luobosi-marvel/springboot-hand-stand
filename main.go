7a65dd12b2246ab9fe1c7df291320c6effca23ca
luobosi <luobosi@2dfire.com>
2018-02-03 11:24:31 +0800

    微调

--- main.go
+++ main.go
@@ -7,20 +7,22 @@ import (
 )
 
 const databaseName  = "marvel"
+
 func main() {
+	//
 	str := "marvel_luobosi_xueliu_naitang"
 
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/marvel?charset=utf8")
 	checkErr(err)
 
 	println(myutil.ToUpperUnderlineWordInitialsForField(str))
-
+	// 查询某个数据库中的所有表名
 	databaseNames, err := myutil.QueryDatabaseTableName(db, databaseName)
 
 	for index, tableName := range databaseNames {
 		println(index, tableName)
 	}
-
+	// 查询表结构信息
 	tableStructures, err := myutil.QueryTableStructure("account")
 
 	if err != nil {
--- myutil/DatabaseUtil.go
+++ myutil/DatabaseUtil.go
@@ -12,11 +12,13 @@ import (
 	return tableStructure	表结构信息
  */
 func QueryTableStructure(tableName string) (tableStructures []allstruct.DataBaseStruct, err error) {
+	// 连接数据库
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/marvel?charset=utf8")
 	defer db.Close()
-
+	// 查询某个表的表结构的 sql
 	sql := "SHOW FULL COLUMNS FROM " + tableName
-
+	// 这里要注意 new 和 make 的区别，make 只能用来为 slice，map 或 chan 类型分配内存和初始化一个对象
+	// new 的作用是初始化一个指向类型的指针
 	tableStructure := new(allstruct.DataBaseStruct)
 	tableStructures = make([]allstruct.DataBaseStruct, 10)
 
@@ -34,9 +36,10 @@ func QueryTableStructure(tableName string) (tableStructures []allstruct.DataBase
 
 /**
 	查询某个数据库中的所有表名
-	param db				操作数据的
-	param databaseName  	数据库的名字
-	return databaseNames[]  该切片包含了数据库里面所有的表名字
+
+	@param db				操作数据的
+	@param databaseName  	数据库的名字
+	@return databaseNames[]  该切片包含了数据库里面所有的表名字
 		   err				错误，如果 err 不为 nil 则说明该方法有错误
  */
 func QueryDatabaseTableName(db *sql.DB, databaseName string) (databaseNames []string, err error) {
